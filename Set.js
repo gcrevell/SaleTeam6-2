9001de878e9e1fa05a1ae0a3a34255e9d5ff2827
gcrevell <gcrevell@mtu.edu>
2014-10-19 15:43:52 -0400

    Added few comments and fixed the end of functions

--- Set.js
+++ Set.js
@@ -2,27 +2,33 @@
  * Created by Wowza on 10/19/14.
  */
 
+/*
+ The element and required methods.
+ */
 function Element(value) {
     this.value = value;
 }
 
 Element.prototype.getElement = function() {
     return eValue;
-}
+};
 
 Element.prototype.setElement = function(newValue) {
     this.value = newValue;
-}
+};
 
+/*
+ The set and required methods.
+ */
 function Set() {
     this.set = new Array();
 }
 
 Set.prototype.addElement = function(newElement) {
     this.set[this.set.length] = newElement;
-}
+};
 
-set.prototype.removeElement = function(element) {
+Set.prototype.removeElement = function(element) {
     for (var i = 0; i < this.set.length; i++) {
         if (this.set[i].value == element.value) {
             var temp = this.set[this.set.length - 1];
@@ -32,26 +38,24 @@ set.prototype.removeElement = function(element) {
             return temp;
         }
     }
-}
+};
 
-set.prototype.getElements = function() {
+Set.prototype.getElements = function() {
     return this.set;
-}
+};
 
-set.prototype.setElements = function(newSet) {
+Set.prototype.setElements = function(newSet) {
     this.set = newSet;
-}
+};
 
-set.prototype.size = function() {
+Set.prototype.size = function() {
     return this.set.size();
-}
+};
 
-set.prototype.clone = function() {
+Set.prototype.clone = function() {
     var returnSet = new Set();
 
     this.set.forEach(function(entry) {
         returnSet.addElement(entry);
     });
-}
-
-var myElement = new Element(5);
+};
